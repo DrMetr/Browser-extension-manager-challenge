1e5dd9c4fce8d7f131de4609a5890c3344132262
Daria M. <metronomedragon@gmail.com>
2025-03-25 15:29:13 +0300

    some work done

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const container = document.querySelector(".extensions_list");
+const allButton = document.querySelector(".all_btn");
+const activeButton = document.querySelector(".active_btn");
+const inactiveButton = document.querySelector(".inactive_btn");
+
 let extensions = [];
 
 fetch("data.json")
@@ -20,18 +24,32 @@ function placeStuff(data){
   let html = '';
   data.forEach(item => {
     html += `
-    <div class="extension is-flex-direction-row grey_background">
-    <img src="${item.logo}">
-    <div class="description is-pulled-right">
-    <h2>${item.name}</h2>
-    <p>${item.description}</p>
+   <div class="extension box is-flex is-flex-direction-column grey_background">
+    <div class="info is-flex is-flex-direction-row mb-6">
+      <img src="${item.logo}" class="is-pulled-left mr-4">
+      <div class="description is-pulled-right">
+        <h2>${item.name}</h2>
+        <p class="has-text-grey-light">${item.description}</p>
+      </div>
     </div>
-    <button class="remove_button is-pulled-left">Remove</button>
+
+    <div class="buttons is-flex is-justify-content-space-between">
+      <button class="remove_button">
+        Remove
+      </button>
     `;
     if (item.isActive == true) {
-      html += `<button class="switch active is-pulled-right"><div class="switch_button is-pulled-right"></div></button></div>`;
+      html +=  `<button class="switch active">
+                        <div class="switch_button on is-pulled-right"></div>
+                      </button>
+                  </div>
+                </div>`;
     } else {
-      html += `<button class="switch inactive is-pulled-right"><div class="switch_button is-pulled-left"></div></button></div>`;
+      html += `<button class="switch inactive">
+                        <div class="switch_button off is-pulled-left"></div>
+                      </button>
+                  </div>
+                </div>`;
     }
   })
 
@@ -43,11 +61,20 @@ function placeStuff(data){
       if (swch.classList.contains("active")){
         swch.classList.replace("active", "inactive");
         swch.firstElementChild.classList.replace("is-pulled-right", "is-pulled-left");
+        swch.firstElementChild.classList.replace("on", "off");
       } else {
         swch.classList.replace("inactive", "active");
         swch.firstElementChild.classList.replace("is-pulled-left", "is-pulled-right");
+        swch.firstElementChild.classList.replace("off", "on");
       }
     })
   })
 }
 
+allButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (allButton.classList.contains("inactive")){
+    allButton.classList.replace("inactive", "active");
+  }
+})
